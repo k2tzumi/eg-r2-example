cebda738bb0ad0d70fcd6b66dbe88215f6add4f5
k2tzumi <katzumi+github@gmail.com>
2024-08-12 20:29:54 +0900

    mod

--- app/Http/Requests/PetRequestBody.php
+++ app/Http/Requests/PetRequestBody.php
@@ -1,14 +1,15 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Requests;
 
 use OpenApi\Attributes as OA;
 
 #[OA\RequestBody(
-    request: "Pet",
-    description: "Pet object that needs to be added to the store",
+    request: 'Pet',
+    description: 'Pet object that needs to be added to the store.',
     required: true,
-    content: new OA\JsonContent(ref: "#/components/schemas/Pet")
+    content: new OA\JsonContent(ref: '#/components/schemas/Pet')
 )]
 class PetRequestBody
 {
